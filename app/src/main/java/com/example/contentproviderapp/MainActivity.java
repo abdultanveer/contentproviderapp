fbeb654b06dab7f0bb273621af511c78e4eab815
abdul <abdul.tanveer@gmail.com>
2019-07-25 21:51:03 +0530

    loaders

--- app/src/main/java/com/example/contentproviderapp/MainActivity.java
+++ app/src/main/java/com/example/contentproviderapp/MainActivity.java
@@ -4,35 +4,55 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CallLog;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    int LOADER_ID = 007;
+    Bundle instructions;
+    ListView listView;
+    SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LoaderManager manager = getSupportLoaderManager();
+        manager.initLoader(LOADER_ID,instructions,this);
         setContentView(R.layout.activity_main);
+         listView = findViewById(R.id.inboxlistview);
 
-        Uri uriSms = Uri.parse("content://sms/inbox");
-        Uri allCalls = Uri.parse("content://call_log/calls");
-
-        ListView listView = findViewById(R.id.inboxlistview);
         String[] from =   new String[]{CallLog.Calls.NUMBER};//name of column in db
         int[] to =  new int[]{android.R.id.text1};
         int layout = android.R.layout.simple_list_item_1;
-
-
-
-        ContentResolver contentResolver = getContentResolver();
-      Cursor dataCursor =
-              contentResolver.query(allCalls,null,null,null,null);
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,layout,dataCursor,from,to);
+         adapter = new SimpleCursorAdapter(this,layout,null,from,to);
         listView.setAdapter(adapter);
+    }
 
 
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        Uri allCalls = Uri.parse("content://call_log/calls");
+        return new CursorLoader(this,
+                allCalls,//warehousse
+                null,null,null,null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+            adapter.swapCursor(cursor);
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
 
     }
 }
